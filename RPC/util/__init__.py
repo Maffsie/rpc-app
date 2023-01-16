2fb60b1acd7a57e26f1ef7b849980112e04a8fff
Maff <maff@maff.im>
2023-01-16 15:14:09 +0000

    oopsie poopsie

--- RPC/util/__init__.py
+++ RPC/util/__init__.py
@@ -1,6 +1,6 @@
 from .base import Base
 from .coercion import coerce_type
 from .conf import preconfigure
-from .decorators import couldError
+from .decorators import throws
 from .graphics import fit_text, render_rdj
 from .log import Logger, LogLevel
