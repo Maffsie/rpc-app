ab1c8a0d22e3f653952ec6671b358f055417a838
Maff <maff@maff.im>
2023-01-23 18:51:18 +0000

    my dude i need to shit

--- RPC/v2/hooks.py
+++ RPC/v2/hooks.py
@@ -1,22 +1,32 @@
 from flask import current_app, request
 
 from RPC.util.base import Api
+from RPC.util.decorators import throws
+from RPC.util.errors import InvalidInputError
 
 routes = Api(url_prefix="/")
 
 
 @routes.route(
-    "/fritzin",
+    "/fritzbox_in",
     methods=[
-        "GET",
         "POST",
     ],
 )
+@throws(
+    InvalidInputError,
+)
 def fritzbox_s2h():
-    current_app.log.verbose(
-        "god damn", args=request.args, method=request.method, body=request.data
+    current_app.log.verbose("fuckin", request.headers)
+    input_json = None
+    try:
+        input_json = request.json
+    except Exception as e:
+        raise InvalidInputError(f"bad boy.........{e}")
+    current_app.log.notice(
+        f"can you believe {input_json['addresses']['from']['address']} would {input_json['subject']}"
     )
     return (
-        f"success? {request.args}",
+        f"thanks for your data it yumy",
         200,
     )
