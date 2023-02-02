30eb4986b392a6cade94b234dc9f542515a8ace7
Maff <maff@maff.im>
2023-02-02 21:18:04 +0000

    v2: Add DVLA to v2 api

--- RPC/v2/__init__.py
+++ RPC/v2/__init__.py
@@ -1,7 +1,8 @@
 from RPC.util.base import Api
 
+from .dvla import routes as dvla_ves
 from .health import routes as health
 from .hooks import routes as hooks
 
 api = Api()
-api.include(health, hooks)
+api.include(dvla_ves, health, hooks)
