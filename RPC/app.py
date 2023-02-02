90de98ddcb7203faff9e6ad6ac7d9dcdc916f6be
Maff <maff@maff.im>
2023-02-02 21:13:04 +0000

    app: Add initial stuff for providers and services

--- RPC/app.py
+++ RPC/app.py
@@ -15,9 +15,22 @@ class RPCApp(Base, Flask):
         "vnd_digitalocean_domain": str,
         "vnd_digitalocean_managed_record": "_tailzone_managed",
     }
+    providers = {}
+    services = {}
 
     def __init__(self, appid: str):
         super().__init__(None, appid)
+        self.setup_providers()
+        self.setup_services()
 
     def include(self, *args):
         [self.register_blueprint(api) for api in args]
+
+    def setup_providers(self):
+        # TODO: this should really be dynamic..
+        from RPC.vnd.dvla import Doovla
+        self.providers["dvla"] = Doovla()
+
+    def setup_services(self):
+        # TODO: dynamic loader for services
+        pass
