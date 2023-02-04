55adb0f47232c22422713c8a43b734cf3347b1a2
Maff <maff@maff.im>
2023-02-04 02:51:59 +0000

    fuck

--- RPC/layers/simple.py
+++ RPC/layers/simple.py
@@ -1,41 +1,6 @@
 import logging
 
-from influxdb import InfluxDBClient
 from requests import Session
-
-from RPC.util.helpers import load_conf_static
-
-
-class WithInfluxDB:
-    db: InfluxDBClient = None
-    db_config = {
-        "data_influx_host": str,
-        "data_influx_port": 8086,
-        "data_influx_tls": False,
-        "data_influx_verifytls": False,
-        "data_influx_auth": bool,
-        "data_influx_user": str,
-        "data_influx_pass": str,
-    }
-
-    def __init__(
-        self, *args, db: str | None = None, dbvar: str | None = None, **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        # TODO: This is messy as fuck
-        if db is None or db == "" and dbvar is not None and dbvar != "":
-            self.db_config[dbvar] = str
-        self.db_config = load_conf_static(self.db_config, {}, {})
-        db = self.db_config.get(dbvar, db)
-        if db is None or db == "":
-            raise Exception("why")
-        self.db = InfluxDBClient(
-            host=self.db_config.get("data_influxdb_host"),
-            port=self.db_config.get("data_influxdb_port"),
-            ssl=self.db_config.get("data_influxdb_tls"),
-            verify_ssl=self.db_config.get("data_influxdb_tlsverify"),
-            database=db,
-        )
 
 
 class WithLogging:
